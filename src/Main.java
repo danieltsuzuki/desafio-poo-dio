bc8ee6d0c1fd8509da1cb5dea7ba1233dd6d51e0
danietsuzuki <massao.tsuzuki@gmail.com>
2023-01-17 15:23:21 -0300

    adicionando classes Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de java");
+        curso1.setDescricao("Java orientado a objetos");
+        curso1.setCargaHoraria(8);
+
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de python");
+        curso2.setDescricao("Introdução a python");
+        curso2.setCargaHoraria(4);
+
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de java");
+        mentoria1.setDescricao("Falaremos sobre conceitos básico de java e orientação a objetos");
+        mentoria1.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
+
     }
+
 }
